e19fe609668bc939057953b8fff6fd3d9002899c
Jon Mattila <jon.mattila@student.laurea.fi>
2026-09-22 16:30:11 +0300

    harjoitus 1 tehty

--- WS03_JS_events/js/script.js
+++ WS03_JS_events/js/script.js
@@ -2,3 +2,38 @@ const AnimalButton = document.getElementById("AnimalButton");
 AnimalButton.addEventListener("click", function() {
 alert("You clicked me!");
 });
+
+function showTable() {
+    const animal1 = "Tiikeri";
+    const habitat1 = "Metsä";
+    const diet1 = "Liha";
+
+    const animal2 = "Norsu";
+    const habitat2 = "Savanni";
+    const diet2 = "Kasvit";
+
+    const table = `
+        <table>
+            <tr>
+                <th>Eläin</th>
+                <th>Elinympäristö</th>
+                <th>Ruokavalio</th>
+            </tr>
+            <tr>
+                <td>${animal1}</td>
+                <td>${habitat1}</td>
+                <td>${diet1}</td>
+            </tr>
+            <tr>
+                <td>${animal2}</td>
+                <td>${habitat2}</td>
+                <td>${diet2}</td>
+            </tr>
+        </table>
+    `;
+
+    const container = document.querySelector("#tableContainer");
+    container.innerHTML = table;
+}
+
+
