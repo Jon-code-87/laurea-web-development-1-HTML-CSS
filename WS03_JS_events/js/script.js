1c6a31a3692118fa726b1917f76c1eeabf359960
Jon Mattila <jon.mattila@student.laurea.fi>
2026-09-22 14:27:48 +0300

    tehty muutoksia

--- WS03_JS_events/js/script.js
+++ WS03_JS_events/js/script.js
@@ -1,4 +1,34 @@
-function showMessage() {
-    alert("You clicked me!");
-}
+const animalButton = document.getElementById("animalButton");
 
+animalButton.addEventListener("click", function() {
+alert("You clicked me!");
+});
+
+const animal = "Tiikeri";
+const habitat = "Metsä";
+const diet = "Liha";
+
+const table = 
+    <table>
+        <tr>
+            <th>Eläin</th>
+            <th>Elinympäristö</th>
+            <th>Ruokavalio</th>
+        </tr>
+        <tr>
+    <td>Norsu</td>
+    <td>Savanni</td>
+    <td>Kasvit</td>
+</tr>
+        <tr>
+            <td>${animal}</td>
+            <td>${habitat}</td>
+            <td>${diet}</td>
+        </tr>
+    </table>
+;
+const tableContainer = document.getElementById("tableContainer");
+const tableButton = document.getElementById("tableButton");
+tableButton.addEventListener("click", function() {
+    tableContainer.innerHTML = table;
+});
