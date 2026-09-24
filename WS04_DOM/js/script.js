932250ed1312559452b35fea8763e6bb0f6af65d
Jon Mattila <jon.mattila@student.laurea.fi>
2026-09-24 14:14:02 +0300

    otsikko muutettu.

---
+++ WS04_DOM/js/script.js
@@ -0,0 +1,6 @@
+const changeHeadingButton = document.querySelector("#changeHeadingButton");
+const taskOneHeading = document.querySelector("#taskOneHeading");
+
+changeHeadingButton.addEventListener("click", function () {
+    taskOneHeading.textContent = "Muokattu otsikko!";
+});
